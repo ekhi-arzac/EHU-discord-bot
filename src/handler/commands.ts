db089ed178b0c3fa0f848147e9225e2923f11e76
ekhi-arzac <105313706+ekhi-arzac@users.noreply.github.com>
2022-12-19 10:34:37 +0100

    prod

--- src/handler/commands.ts
+++ src/handler/commands.ts
@@ -1,5 +1,4 @@
 import { Command } from "../type/command";
 import { Test } from "../commands/test";
-import { Inkesta } from "../commands/inkesta";
 
-export const Commands: Command[] = [Test, Inkesta];
+export const Commands: Command[] = [Test];
